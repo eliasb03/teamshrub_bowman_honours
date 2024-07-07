81a83cf57917324880c1cf61e57a43d9dfc59c5b
eliasb03 <eliasbowmanschool@gmail.com>
2024-07-06 18:43:29 -0600

    Initial commit of empty script to create spectorgrams from audio

---
+++ scripts/0X_spectrogram_v0.R
@@ -0,0 +1,8 @@
+#------------------------------
+# teamshrub_bowman_honours
+# 0X_spectrogram_v0
+# By: Elias Bowman 
+# 2024-07-05
+# Description: This script will do early work with audio files from ARUs. I want this script to scan through one audio file, create subsamples and visualize it. Then I will need a way to select specific moments of noise and create a spectrogram.
+#  
+#------------------------------
