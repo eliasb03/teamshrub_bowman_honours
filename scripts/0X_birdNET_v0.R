9f1ac2f915ea76b714b1cb134acdd8a2c27c7514
eliasb03 <eliasbowmanschool@gmail.com>
2024-07-07 21:11:31 -0600

    very initial start to implementing BirdNET in R

---
+++ scripts/0X_birdNET_v0.R
@@ -0,0 +1,14 @@
+#------------------------------
+# teamshrub_bowman_honours
+# 0X_birdNET_v0
+# By: Elias Bowman 
+# 2024-07-05
+# Description: This script will install and start working with BirdNET the cornell lab bird id software
+#  
+#------------------------------
+
+# importing packages
+devtools::install_github('nationalparkservice/NSNSDAcoustics')
+
+
+
