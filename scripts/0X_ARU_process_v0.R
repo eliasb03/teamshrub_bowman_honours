3115d79ea6b53246f0722c0c3177860db430b348
eliasb03 <eliasbowmanschool@gmail.com>
2024-09-17 21:38:20 -0700

    initial commit for ARU_process

---
+++ scripts/0X_ARU_process_v0.R
@@ -0,0 +1,13 @@
+#------------------------------
+# teamshrub_bowman_honours
+# 0X_ARU_process_v0
+# By: Elias Bowman 
+# Created: 2024-09-07
+# 
+# Description: This script will use the birdNET_analyze function created in the birdNET script
+# This script will lead to the creation of outputted song recognition data based on ARU recordings
+#
+#------------------------------
+
+
+  
