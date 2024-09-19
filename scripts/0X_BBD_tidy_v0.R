0354412d17858f01138f80f1558d97452f551a96
eliasb03 <eliasbowmanschool@gmail.com>
2024-09-19 14:06:51 -0700

    Initial commit of Breeding Bird Data tidying script

---
+++ scripts/0X_BBD_tidy_v0.R
@@ -0,0 +1,11 @@
+#------------------------------
+# teamshrub_bowman_honours
+# 0X_BBD_Tidy_v0
+# By: Elias Bowman 
+# Created: 2024-09-19
+# 
+# Description: This script will import and tidy the Ranger collected Breeding Bird Data from Qikiqtaruk - Herschel Island Territorial Park 
+#
+#------------------------------
+
+
