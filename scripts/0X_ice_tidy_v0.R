56c3724109100f68325ec60f00ecc9d0b51fc112
eliasb03 <eliasbowmanschool@gmail.com>
2024-10-15 13:54:54 -0700

    Removed trailing legend lines and formatted week column as date

--- scripts/0X_ice_tidy_v0.R
+++ scripts/0X_ice_tidy_v0.R
@@ -10,6 +10,7 @@
 
 # Import Packages
 library(dplyr)
+library(lubridate)
 
 # Function to import ice data
 import_ice_data <- function(file_path) {
@@ -28,8 +29,13 @@ import_ice_data <- function(file_path) {
     gsub("[^a-z0-9_]", "", .) %>%
     gsub("ct", "conc", .)
   
-  # Import data 
-  data <- read.csv(file_path, skip = 10, header = FALSE, col.names = colnames)
+  # Determine number of rows in the file
+  total_rows <- length(readLines(file_path))
+  
+  # Import data into the script, excluding footer lines
+  data <- read.csv(file_path, skip = 10, header = FALSE, col.names = colnames, nrows = total_rows - 18) # 18 seems to be the number of post-data legend lines there are
+  
+  data$week <- ymd(data$week)
   
   return(data)
 }
