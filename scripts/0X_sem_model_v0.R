4e5a35bb759ed63191863ea55899656ac918ff04
eliasb03 <eliasbowmanschool@gmail.com>
2024-10-16 10:49:43 -0700

    created new blank script to run SEM model

---
+++ scripts/0X_sem_model_v0.R
@@ -0,0 +1,14 @@
+#------------------------------
+# teamshrub_bowman_honours
+# 0X_sem_model_v0
+# By: Elias Bowman 
+# Created: 2024-10-16
+# Last update: 2024-10-16
+# 
+# Description: This script will construct and run SEM models for my analysis
+# At this time (16Oct2024) I am not ready to run my model at all, but I think it is valuable to make a code framework to do so 
+#------------------------------
+
+# Import Packages 
+library(lavaan)
+library(semPlot)
