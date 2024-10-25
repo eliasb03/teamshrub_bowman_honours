99b43d84e683927d9c037cae9fe6b6a337cffde6
eliasb03 <eliasbowmanschool@gmail.com>
2024-10-25 16:52:48 -0700

    added prelim assessments of data coverage

--- scripts/sem_analysis/0X_BBS_prelim_v0.R
+++ scripts/sem_analysis/0X_BBS_prelim_v0.R
@@ -8,6 +8,42 @@
 #
 #------------------------------
 
+# find list of years included in the bbs.survey dataset
+list_of_years <- bbs.survey %>%
+  distinct(year) %>%
+  pull(year) %>%
+  unique()
+
+years_with_both <- bbs.survey %>%
+  group_by(year) %>%
+  filter(n_distinct(period) == 2) %>%
+  distinct(year) %>%
+  pull(year)
+
+years_with_one <- setdiff(list_of_years, years_with_both)
+
+length(years_with_both)
+length(years_with_one)
+
+
+##### SIMPLE TEMPORARY PLOTTING CODE TO VISUALIZE CHANGE BETWEEN EARLY AND LATE SURVEYS
+species_data <- bbs.long %>%
+  filter(species == "Common Eider") %>%
+  group_by(year, period) %>%  # Group by year and period
+  summarise(count = n(), .groups = 'drop') %>%  # Summarize to count observations
+  arrange(year, period)  # Arrange for plotting
+
+# Plot
+
+ggplot(species_data, aes(x = period, y = count, group = year)) +
+  geom_point(aes(color = as.factor(year)), size = 3) +  # Points for EARLY and LATE
+  geom_line(aes(group = year, color = as.factor(year)), na.rm = TRUE) +  # Lines between EARLY and LATE
+  theme_minimal() +
+  labs(x = "Survey Period", 
+       y = "Number of Birds Observed", 
+       title = "Bird Observations for Species X in EARLY vs LATE Surveys",
+       color = "Year") +
+  theme(legend.position = "bottom")
 
 # Create the line plot
 ggplot(spec.year.sum, aes(x = year, y = total.observations, color = species)) +
