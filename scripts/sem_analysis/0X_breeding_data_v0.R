893e35966f2b3413b2a2ab212c9717a8590d992c
eliasb03 <eliasbowmanschool@gmail.com>
2024-11-02 15:41:22 -0700

    Elias created a new script to select only breeding records from BBS and ebird data

---
+++ scripts/sem_analysis/0X_breeding_data_v0.R
@@ -0,0 +1,9 @@
+#------------------------------
+# teamshrub_bowman_honours
+# 0X_breeding_data_v0
+# By: Elias Bowman 
+# Created: 2024-11-02
+# 
+# Description: This script will create a dataset of all breeding records from the Breeding Bird and Ebird Datasets on Qikiqtaruk - Herschel Island
+#
+#------------------------------
