86995910f3d3e4cb376ea51d756cdb8c8e62ef1a
eliasb03 <eliasbowmanschool@gmail.com>
2024-11-28 19:07:48 -0800

    Elias carried over variance in estimates

--- scripts/sem_analysis/0X_snow_and_budburst_v0.R
+++ scripts/sem_analysis/0X_snow_and_budburst_v0.R
@@ -16,21 +16,19 @@
 library(stats)
 
 
-combine_sd <- function(sds) {
-  sd_val <- sqrt(sum(sds^2) / length(sds))
-  return(sd_val)
-}
 
 # Phenology
-qiphen <- read.csv("data/raw/qiki_phen_with_before_2022.csv", stringsAsFactors = F)
-# graphs
+qiphen <- read.csv("data/raw/qiki_phen_with_before_2024_partial.csv", stringsAsFactors = F)
 
 snowmelt <- qiphen %>% select(Year, Spp, Plot.ID, P1) %>% na.omit()
 SALARC_budburst <- qiphen %>% filter(Spp == "SALARC") %>% select(Year, Plot.ID, P2) %>% na.omit()
 
-
-
 SALARC_budburst_avg <- SALARC_budburst %>% na.omit() %>% group_by(Year) %>% summarise(P2_mean = mean(P2), P2_sd = sd(P2))
+
+combine_sd <- function(sds) {
+  sd_val <- sqrt(sum(sds^2) / length(sds))
+  return(sd_val)
+}
 
 snowmelt_avg <- snowmelt %>% na.omit() %>% group_by(Year, Spp) %>% summarise(P1_mean = mean(P1), P1_sd = sd(P1)) %>% group_by(Year) %>% summarise(P1_mean = mean(P1_mean), P1_sd = combine_sd(P1_sd))
 
