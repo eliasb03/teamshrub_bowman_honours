68307bb7fc6e5846f2fc9b9f4236df98db84b29d
eliasb03 <eliasbowmanschool@gmail.com>
2025-02-09 12:29:16 -0800

    Elias created a nest_tidy script

---
+++ scripts/sem_analysis/01b_nest_tidy_v0.R
@@ -0,0 +1,16 @@
+#------------------------------
+# teamshrub_bowman_honours
+# 0X_nests_tidy_v0
+# By: Elias Bowman 
+# Created: 2025-01-21
+#
+# Description: This script imports, tidies, and summarizes the annotated breeding bird data for the Qikiqtaruk - Herschel Island site. This script will import and tidy the very messy dataset of BBS and Ebird data that Elias annotated
+#------------------------------
+
+# Load necessary libraries
+library(tidyverse)
+
+# Importing datasets
+breeding_data_path <- "data/"
+breeding_data <- read_csv()
+
