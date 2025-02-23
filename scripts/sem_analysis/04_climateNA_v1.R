00658238e2a6d41258fcaa56837b17462e700a5b
eliasb03 <eliasbowmanschool@gmail.com>
2025-02-22 21:42:35 -0800

    Elias added a more precise breeding season climate variable using on downloads from the Komakuk climate station

--- scripts/sem_analysis/04_climateNA_v1.R
+++ scripts/sem_analysis/04_climateNA_v1.R
@@ -92,7 +92,11 @@ input_df <- create_input_df(latitude, longitude, elevation)
 # Define file paths
 input_file_path <- "data/raw/climateNA/climateNA_input.csv"
 output_dir <- "data/raw/climateNA/output/"
+station_data_dir <- "data/raw/komakuk_climate_data/"
 
+##############################
+# ClimateNA Processing
+##############################
 # Write input file
 write_csv(input_df, input_file_path)
 message("Input file written to: ", input_file_path)
@@ -111,17 +115,102 @@ message("Output directory: ", output_dir)
 run_climateNA_sequential(input_file_path, year_inputs, clim_var_list, output_dir)
 
 # Process ClimateNA output files
-climate_data <- process_climateNA_files(output_dir)
+climate_dataNA <- process_climateNA_files(output_dir)
 
 # Remove repeated rows
-climate_data <- climate_data[!duplicated(climate_data), ]
+climate_dataNA <- climate_dataNA[!duplicated(climate_dataNA), ]
 
-# View resulting dataframe
-print(climate_data)
+message("ClimateNA processing script completed successfully.")
 
-# Save resulting dataframe
+#########################
+# Komakuk Climate Station
+#########################
+# Bash code to download komakuk daily weather data
+# for year in $(seq 1995 2025); do
+# wget --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=10822&Year=${year}&Month=5&Day=14&timeframe=2&submit= Download+Data";
+# done;
+
+
+# Function to read, mutate, and combine climate data
+join_climate_data <- function(data_dir) {
+  # Get list of climate files
+  climate_files <- list.files(data_dir, pattern = "\\.csv$", full.names = TRUE)
+  
+  # Initialize an empty list to store mutated dataframes
+  data_list <- list()
+  
+  # Loop through each file and process the data
+  for (file in climate_files) {
+    df <- read_csv(file)
+    
+    # Rename columns to remove spaces
+    colnames(df) <- gsub("[[:space:]]", "_", colnames(df))  # Replace spaces with underscores
+    colnames(df) <- gsub("[()°/]", "", colnames(df))          # Remove parentheses and degree symbols
+    
+    
+    
+    # Mutate to create new columns based on existing ones
+    df_new <- df %>%
+      mutate(
+        longitude = as.numeric(Longitude_x),  
+        latitude = as.numeric(Latitude_y),
+        stn_name = Station_Name,
+        date_time = ymd(DateTime),
+        year = as.numeric(Year),
+        month = as.numeric(Month),
+        day = as.numeric(Day),
+        mean_temp = as.numeric(Mean_Temp_C),
+        max_temp = as.numeric(Max_Temp_C),
+        min_temp = as.numeric(Min_Temp_C)
+      ) %>%
+      select(longitude, latitude, stn_name, date_time, year, month, day, mean_temp, max_temp, min_temp)
+    
+    
+    data_list[[file]] <- df_new
+  }
+  
+  # Combine all mutated dataframes into one
+  combined_data <- bind_rows(data_list)
+  
+  return(combined_data)
+}
+
+komakuk_data <- join_climate_data(station_data_dir)
+
+# averaging to the monthly level
+komakuk_monthly <- komakuk_data %>%
+  group_by(year, month) %>%
+  summarise(
+    mean_temp = mean(mean_temp, na.rm = TRUE),
+    max_temp = mean(max_temp, na.rm = TRUE),
+    min_temp = mean(min_temp, na.rm = TRUE)
+  )
+
+komakuk_breed_season <- komakuk_data %>%
+  # Filter for the date range (April 15 to June 30)
+  filter(date_time >= as.Date(paste(year, 4, 15, sep = "-")) & date_time <= as.Date(paste(year, 5, 30, sep = "-"))) %>%
+  group_by(year) %>%
+  summarise(
+    count_mean_temp = sum(!is.na(mean_temp)),  # Count non-NA values for mean_temp
+    mean_temp_breeding_season = mean(mean_temp, na.rm = TRUE),
+    max_temp_breeding_season = mean(max_temp, na.rm = TRUE),
+    min_temp_breeding_season = mean(min_temp, na.rm = TRUE)
+  ) %>% 
+  filter(count_mean_temp > 40)  # Filter out years with little or no data
+
+
+#########################
 # Saving climate data
+#########################
+
+climate_data <- climate_dataNA %>%
+  left_join(komakuk_breed_season, by = join_by(year)) %>%
+  select(-lat, -lon, -elev, -count_mean_temp) %>%
+  select(year, everything())
+
+
 output_path <- "data/clean/sem/" # Output path
 write_csv(climate_data, paste0(output_path, "climate_data.csv"))
 
-message("ClimateNA processing script completed successfully.")
+
+
